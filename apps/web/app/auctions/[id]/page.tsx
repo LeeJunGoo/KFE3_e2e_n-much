8739ec0487d9d121f83e991ab851acca69af5eaf
LeeJnuGoo <leejungoo1396@gmail.com>
2025-06-17 22:52:05 +0900

    chore: 리엑트 아이콘 라이브러리 설치 (#9)

---
+++ apps/web/app/auctions/[id]/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const page = () => {
+  return (
+    <>
+      <header>
+        <button className="flex">
+          <p>아이콘</p>
+          <p>경매 목록으로 돌아가기</p>
+        </button>
+      </header>
+      경매 상품 상세 page
+    </>
+  );
+};
+
+export default page;
