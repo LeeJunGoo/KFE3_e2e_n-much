3946f89a3d8001ed83adb0cc5fad1cb03f1aef97
Seoyoung Park <seokachuu@gmail.com>
2025-06-18 03:07:52 +0900

    feat: add pretendard font, metadata 분리

---
+++ apps/web/app/metadata/auctions/write.ts
@@ -0,0 +1,10 @@
+export const write = {
+  title: '경매 작성 페이지 -Vidding',
+  description: '경매를 통한 가치를 팔자! 경매를 통한 가치를 사자!',
+  openGraph: {
+    title: '경매 작성 페이지 -Vidding',
+    description: '경매를 통한 가치를 팔자! 경매를 통한 가치를 사자!',
+    images: [''],
+    type: 'website'
+  }
+};
