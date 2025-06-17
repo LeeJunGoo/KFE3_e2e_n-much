e5504c9520256acd96031798ecf752663a00640a
Seoyoung Park <seokachuu@gmail.com>
2025-06-18 04:51:31 +0900

    chore: add tanstack query (#7)

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,6 +1,7 @@
 import PageContainer from '@repo/ui/components/layout/PageContainer';
 import { pretendard } from './fonts';
 import { home } from '../app/metadata/common/home';
+import QueryProvider from './providers/QueryProvider';
 import './globals.css';
 
 export const metadata = home;
@@ -9,9 +10,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="ko">
       <body className={`flex flex-col w-full max-w-2xl m-auto shadow-2xl relative ${pretendard.className}`}>
-        <header className="w-full bg-blue-400 h-16 px-5">헤더테스트</header>
-        <PageContainer>{children}</PageContainer>
-        <nav className="fixed bottom-0 left-0 right-0 max-w-2xl bg-amber-300 m-auto h-16">네비테스트</nav>
+        <QueryProvider>
+          <header className="w-full bg-blue-400 h-16 px-5">헤더테스트</header>
+          <PageContainer>{children}</PageContainer>
+          <nav className="fixed bottom-0 left-0 right-0 max-w-2xl bg-amber-300 m-auto h-16">네비테스트</nav>
+        </QueryProvider>
       </body>
     </html>
   );
