d76f8b941b07038ed390988129f6a0e2c918e374
baram55 <asd7973@gmail.com>
2025-06-17 15:05:03 +0900

    feat: 제목, 주소, 상세 내용 Input 필드 추가 (#8)

--- packages/ui/src/components/write-auction.tsx
+++ packages/ui/src/components/write-auction.tsx
@@ -9,8 +9,17 @@ import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, For
 import { Input } from './ui/input';
 
 const formSchema = z.object({
-  username: z.string().min(2, {
-    message: 'Username must be at least 2 characters.'
+  title: z
+    .string()
+    .min(5, {
+      message: '경매 제목은 최소 5자가 되어야 합니다.'
+    })
+    .max(50, {
+      message: '경매 제목은 최대 50자가 되어야 합니다.'
+    }),
+  address: z.string().min(5, { message: '주소는 최소 5글자가 되어야 합니다.' }),
+  description: z.string().min(5, { message: '상세 내용은 최소 5글자가 되어야 합니다.' }).max(500, {
+    message: '상세 내용은 최대 500자가 되어야 합니다.'
   })
 });
 
@@ -18,7 +27,9 @@ export function AuctionForm() {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      username: ''
+      title: '',
+      address: '',
+      description: ''
     }
   });
 
@@ -34,14 +45,39 @@ export function AuctionForm() {
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
         <FormField
           control={form.control}
-          name="username"
+          name="title"
           render={({ field }) => (
             <FormItem>
-              <FormLabel>Username</FormLabel>
+              <FormLabel>제목</FormLabel>
               <FormControl>
-                <Input placeholder="shadcn" {...field} />
+                <Input placeholder="경매 상품의 제목을 입력하세요." {...field} />
               </FormControl>
-              <FormDescription>This is your public display name.</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="address"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>주소</FormLabel>
+              <FormControl>
+                <Input placeholder="상품 위치 또는 주소를 입력하세요." {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="description"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>상세 내용</FormLabel>
+              <FormControl>
+                <Input placeholder="상품에 대한 자세한 설명을 입력하세요." {...field} />
+              </FormControl>
               <FormMessage />
             </FormItem>
           )}
