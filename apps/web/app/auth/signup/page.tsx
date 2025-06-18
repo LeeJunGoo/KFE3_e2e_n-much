cf9c00fd223604a7b6a11496c26e7a6da6b8a172
kimsh0306 <b666790@gmail.com>
2025-06-18 22:50:04 +0900

    feat: 회원가입페이지 소셜 로그인 기능 1차 구현

--- apps/web/app/auth/signup/page.tsx
+++ apps/web/app/auth/signup/page.tsx
@@ -1,10 +1,31 @@
 "use client";
+import { useEffect } from 'react';
+import { createClient } from '@repo/ui/utils/supabase/client/client';
+import { useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation";
 import PageTitle from '@repo/ui/components/typography/PageTitle';
 
 export default function SignupPage() {
-  const handleSocialSignup = (provider: string) => {
-    // 소셜 회원가입 로직
+  const supabase = createClient();
+  const searchParams = useSearchParams();
+  const router = useRouter();
+
+  const handleSocialSignup = async (provider: "google" | "kakao") => {
+    await supabase.auth.signInWithOAuth({
+      provider,
+      options: {
+        // scopes: "email profile",
+        redirectTo: "http://localhost:3001/auth/signup",
+      }
+    });
   };
+
+  useEffect(() => {
+    if (searchParams.get("code")) {
+      // 쿼리 파라미터 code 삭제
+      router.replace("/");
+    }
+  }, [router, searchParams]);
 
   return (
     <>
