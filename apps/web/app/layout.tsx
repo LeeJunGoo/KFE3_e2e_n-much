954b805d253698352afa3ed69bfa197c3452c097
AhnJUWon <urams79@gmail.com>
2025-06-18 16:49:26 +0900

    style : Navbar UI 구성 (#12)

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Geist } from 'next/font/google';
 import PageContainer from '@repo/ui/components/layout/PageContainer';
-import Navbar from '@repo/ui/components/navbar/navbar';
+import Navbar from '@repo/ui/components/navbar/Navbar';
 
 const geist = Geist({ subsets: ['latin'] });
 
@@ -17,9 +17,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body className={`flex flex-col w-full max-w-2xl m-auto shadow-2xl relative ${geist.className}`}>
         <header className="w-full bg-blue-400 h-16 px-5">헤더테스트</header>
         <PageContainer>{children}</PageContainer>
-        <nav className="fixed bottom-0 left-0 right-0 max-w-2xl bg-amber-300 m-auto h-16">
-          <Navbar />
-        </nav>
+        <Navbar />
       </body>
     </html>
   );
