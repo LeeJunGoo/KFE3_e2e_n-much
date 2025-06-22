e6b38c6385071bc7c0fe621a37c7a20c0df0c4a5
LeeJnuGoo <leejungoo1396@gmail.com>
2025-06-23 01:17:33 +0900

    Merge pull request joo1e/feature/TabBar-#25

---
+++ apps/web/src/app/layout.tsx
@@ -0,0 +1,24 @@
+import QueryProvider from '../../providers/QueryProvider';
+
+import { home } from '../../metadata/common/home';
+import { pretendard } from '../../fonts';
+
+import './globals.css';
+import TabBar from '@repo/ui/components/common/TabBar';
+import PageContainer from '@repo/ui/components/layout/PageContainer';
+
+export const metadata = home;
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="ko">
+      <body className={`flex flex-col w-full max-w-2xl m-auto shadow-2xl relative ${pretendard.className}`}>
+        <QueryProvider>
+          {/* <header className="w-full bg-blue-400 h-16 px-5">헤더테스트</header> */}
+          <PageContainer>{children}</PageContainer>
+          <TabBar />
+        </QueryProvider>
+      </body>
+    </html>
+  );
+}
