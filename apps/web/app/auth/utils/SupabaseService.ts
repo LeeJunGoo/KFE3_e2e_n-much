fa4fbf081c80ff57dde7b6c2f8273fc0578f45e5
kimsh0306 <b666790@gmail.com>
2025-06-23 14:02:04 +0900

    feat: 기존 유저 데이터가 DB에 있을 시, 기존 role 값으로 덮어쓰기 (#16)

--- apps/web/app/auth/utils/SupabaseService.ts
+++ apps/web/app/auth/utils/SupabaseService.ts
@@ -1,6 +1,5 @@
 import { createClient } from '@repo/ui/utils/supabase/client/client';
 import { upsertUser } from '@repo/ui/utils/supabase/query/users';
-import { handleSupabaseError } from './SupabaseError';
 
 type Role = 'BUYER' | 'SELLER';
 type Provider = 'google' | 'kakao';
@@ -8,36 +7,49 @@ type Provider = 'google' | 'kakao';
 const supabase = createClient();
 
 export const storeUserInfo = async (role: Role) => {
-  // supabase.auth.getUser()
-  // ==> 현재 로그인 세션(localStorage)에 남아 있는 토큰을 기준으로, 로그인된 유저 정보를 가져옴
   const {
     data: { user }
   } = await supabase.auth.getUser();
 
-  if (user) {
-    console.log(user);
-    try {
-      const userData = await upsertUser({
-        user_id: user.id,
-        email: user.email ?? '',
-        nickname: user.user_metadata?.name ?? '',
-        avatar: user.user_metadata?.avatar_url ?? null,
-        created_at: user.created_at ?? new Date().toISOString(),
-        updated_at: user.updated_at ?? new Date().toISOString(),
-        password: '', // 소셜가입이므로 빈값 허용 (아예 안 쓰려면 null 가능하게 테이블 구조 변경 필요)
-        point: 0,
-        role: role,
-        favorites: []
-      });
-      if (userData) {
-        console.log(userData.nickname, '님, 사용자 정보 저장 성공');
-      }
-    } catch (error: unknown) {
-      handleSupabaseError(error);
-      console.log('사용자 정보 저장 실패: ' + error);
-    }
-  } else {
+  if (!user) {
     console.log('getUser 실패');
+    return;
+  }
+
+  let roleToSave: Role = role;
+
+  // 현재 유저가 DB에 있는지 체크
+  const { data: dbUser } = await supabase.from('users').select('role').eq('user_id', user.id).single();
+
+  // 저장된 유저의 role 값으로 덮어쓰기
+  if (dbUser && dbUser.role !== role) {
+    roleToSave = dbUser.role as Role;
+    console.log(`기존에 ${roleToSave}로 저장된 USER입니다.`);
+  }
+
+  try {
+    const userData = await upsertUser({
+      user_id: user.id,
+      email: user.email ?? '',
+      nickname: user.user_metadata?.name ?? '',
+      avatar: user.user_metadata?.avatar_url ?? null,
+      created_at: user.created_at ?? new Date().toISOString(),
+      updated_at: user.updated_at ?? new Date().toISOString(),
+      password: '',
+      point: 0,
+      role: roleToSave,
+      favorites: []
+    });
+
+    if (userData) {
+      console.log(`${userData.nickname} 님, 사용자 정보 저장 성공`);
+    }
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.error(error.message);
+    } else {
+      console.error('알 수 없는 오류가 발생했습니다.');
+    }
   }
 };
 
