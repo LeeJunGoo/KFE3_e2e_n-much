7ddf5dd9f1b74aaea33bcf8666b1141a5ee5441c
Seoyoung Park <seokachuu@gmail.com>
2025-06-25 03:00:41 +0900

    feat: 디자인 토큰 추가

--- apps/web/src/app/mypage/page.tsx
+++ apps/web/src/app/mypage/page.tsx
@@ -6,11 +6,11 @@ const MyPage = () => {
   return (
     <>
       <PageTitle>마이페이지</PageTitle>
-      <section className="rounded-2xl p-4 mt-6 bg-indigo-50">
-        <div className="flex gap-4 items-center">
-          <div className="size-20 rounded-full flex items-center justify-center relative shrink-0">
-            <div className="absolute inset-0 bg-white opacity-50 rounded-full" aria-hidden="true" />
-            <div className="relative z-10 overflow-hidden rounded-full border border-gray-300 size-16 bg-white">
+      <section className="mt-6 rounded-2xl bg-(--color-primary) p-4">
+        <div className="flex items-center gap-4">
+          <div className="relative flex size-20 shrink-0 items-center justify-center rounded-full">
+            <div className="absolute inset-0 rounded-full bg-white opacity-50" aria-hidden="true" />
+            <div className="relative z-10 size-16 overflow-hidden rounded-full border border-gray-300 bg-white">
               <Image
                 src="https://api.dicebear.com/7.x/avataaars/svg?seed=11"
                 alt="User Avatar"
@@ -21,8 +21,8 @@ const MyPage = () => {
               />
             </div>
           </div>
-          <div className="flex flex-col gap-0.5 text-sm w-full relative">
-            <Link href="/mypage/profile" className="absolute right-0 top-2/4 -translate-y-2/4">
+          <div className="relative flex w-full flex-col gap-0.5 text-sm">
+            <Link href="/mypage/profile" className="absolute top-2/4 right-0 -translate-y-2/4">
               아이콘
             </Link>
             <h3 className="text-lg font-semibold">안주원</h3>
@@ -31,7 +31,7 @@ const MyPage = () => {
             <p className="text-gray-400">서울특별시 서대문구</p>
           </div>
         </div>
-        <div className="flex gap-3 items-center mt-5 pt-5 border-t border-t-gray-300">
+        <div className="mt-5 flex items-center gap-3 border-t border-t-gray-300 pt-5">
           <p className="text-4xl">⭐️</p>
           <div className="flex flex-col">
             <h3>입찰 참여자</h3>
@@ -40,29 +40,29 @@ const MyPage = () => {
         </div>
       </section>
       <nav className="mt-11">
-        <ul className="rounded-2xl border bg-amber-100">
+        <ul className="rounded-2xl border bg-(--color-primary)">
           <li className="border-b border-b-gray-300">
-            <Link href="/mypage/my-auctions" className="flex justify-between w-full p-4">
+            <Link href="/mypage/my-auctions" className="flex w-full justify-between p-4">
               <p>내 경매 현황</p>
               <p>아이콘</p>
             </Link>
           </li>
           <li className="border-b border-b-gray-300">
-            <Link href="/mypage/points" className="flex justify-between w-full p-4">
+            <Link href="/mypage/points" className="flex w-full justify-between p-4">
               <p>포인트 사용 내역</p>
               <p>아이콘</p>
             </Link>
           </li>
           <li className="border-b border-b-gray-300">
-            <Link href="/mypage/posts" className="flex justify-between w-full p-4">
+            <Link href="/mypage/posts" className="flex w-full justify-between p-4">
               <p>내가 쓴 글</p>
               <p>아이콘</p>
             </Link>
           </li>
         </ul>
       </nav>
-      <div className="w-full flex justify-center items-center mt-11">
-        <button className="text-gray-400 text-sm cursor-pointer">로그아웃</button>
+      <div className="mt-11 flex w-full items-center justify-center">
+        <button className="cursor-pointer text-sm text-gray-400">로그아웃</button>
       </div>
     </>
   );
