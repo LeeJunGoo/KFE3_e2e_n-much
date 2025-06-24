ae7c6253240939f70f7075d806f0981e3c7eeeb0
baram55 <asd7973@gmail.com>
2025-06-24 16:24:08 +0900

    chore: tailwind className 자동 정렬 패키지 설치 및 설정 (#39)

--- apps/web/src/app/auctions/write/page.tsx
+++ apps/web/src/app/auctions/write/page.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from 'react';
 import AuctionForm from 'src/components/auctions/AuctionForm';
 
 export default function Page() {
-  return <AuctionForm />;
+  return (
+    <Suspense fallback={<p>Loading...(useSearchParams)</p>}>
+      <AuctionForm />
+    </Suspense>
+  );
 }
