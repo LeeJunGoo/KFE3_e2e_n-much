1abb71817fb428d346f6e165b416ed972b8f5673
baram55 <asd7973@gmail.com>
2025-06-24 16:28:43 +0900

    chore: shadcn Select 패키지 추가 (#39)

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -2,12 +2,12 @@ import AuctionCard from 'src/components/common/AuctionCard';
 
 export default function Home() {
   return (
-    <main className="min-h-screen p-10 bg-gray-50">
-      <div className="flex flex-wrap gap-6">
+    <main className="min-h-screen bg-gray-50">
+      <ul className="flex flex-wrap gap-6">
         <AuctionCard />
         <AuctionCard />
         <AuctionCard />
-      </div>
+      </ul>
     </main>
   );
 }
