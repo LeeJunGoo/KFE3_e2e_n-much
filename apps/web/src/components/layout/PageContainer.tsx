40d4ad9fce9145f764c4215f9dad324f753b5aba
Seoyoung Park <seokachuu@gmail.com>
2025-06-26 04:30:39 +0900

    chore: add tailwind-merge (#47)

--- apps/web/src/components/layout/PageContainer.tsx
+++ apps/web/src/components/layout/PageContainer.tsx
@@ -1,3 +1,5 @@
+import { twMerge } from 'tailwind-merge';
+
 interface PageContainerProps {
   children: React.ReactNode;
   className?: string;
@@ -5,7 +7,9 @@ interface PageContainerProps {
 
 const PageContainer = ({ children, className = '' }: PageContainerProps) => {
   return (
-    <main className={`min-h-[calc(100vh-4rem)] bg-(--color-background) px-5 pt-8 pb-20 ${className} `}>{children}</main>
+    <main className={twMerge('min-h-[calc(100vh-4rem)] bg-(--color-background) px-5 pt-8 pb-20', className)}>
+      {children}
+    </main>
   );
 };
 
