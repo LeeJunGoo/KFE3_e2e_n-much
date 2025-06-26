f1277a88882fa57332005886d62baf5eead9f916
Seoyoung Park <seokachuu@gmail.com>
2025-06-27 08:37:19 +0900

    feat: header logo image 추가 (#49)

--- apps/web/src/components/layout/Header.tsx
+++ apps/web/src/components/layout/Header.tsx
@@ -1,15 +1,25 @@
+import Image from 'next/image';
 import React from 'react';
-import { FaBell } from 'react-icons/fa';
+import { IoNotifications } from 'react-icons/io5';
+import Logo from '../../../assets/images/logo.svg';
+import Link from 'next/link';
 
 const Header = () => {
   return (
-    <header className="w-full bg-blue-400 h-16 px-5">
-      <ul className="h-full flex items-center justify-between text-white ">
-        <div className="text-lg font-bold">LOGO</div>
-        <button className="relative">
-          <FaBell size={20} />
-        </button>
-      </ul>
+    <header className="h-16 w-full border-b border-b-(--color-light-gray)/30 px-5">
+      <div className="flex h-full items-center justify-between">
+        <Link href="/">
+          <h1 className="text-lg font-bold">
+            <Image src={Logo} alt="logo" className="size-12" />
+          </h1>
+        </Link>
+        <div className="flex items-center">
+          <button className="relative">
+            <IoNotifications size={24} className="text-(--color-accent)" />
+            <span className="absolute top-0 right-0.5 size-2 rounded-full bg-(--color-red)" />
+          </button>
+        </div>
+      </div>
     </header>
   );
 };
