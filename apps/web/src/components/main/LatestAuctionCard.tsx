40e91962e37a806d48fe919dbc2a1ff895ee2e0f
LeeJnuGoo <leejungoo1396@gmail.com>
2025-06-27 13:39:28 +0900

    feat: 카드 클릭 시, 경매 상세페이지로 이동 (#54)

--- apps/web/src/components/main/LatestAuctionCard.tsx
+++ apps/web/src/components/main/LatestAuctionCard.tsx
@@ -7,6 +7,7 @@ import { ko } from 'date-fns/locale';
 import { TZDate } from 'react-day-picker';
 import { Badge } from '@repo/ui/components/ui/badge';
 import { FaBookOpen, FaHeart } from 'react-icons/fa6';
+import Link from 'next/link';
 
 const LatestAuctionCard = ({ auction }: { auction: SortedAuctionItemType }) => {
   const auctionImage = auction.image_urls && auction.image_urls.length > 0 ? auction.image_urls[0] : NotAuctionImage;
@@ -21,40 +22,45 @@ const LatestAuctionCard = ({ auction }: { auction: SortedAuctionItemType }) => {
   const episodesCount = auction.episodes && auction.episodes.length > 0 ? auction.episodes.length : 0;
 
   return (
-    <li className="flex cursor-pointer items-center p-3 transition-colors hover:bg-(--color-secondary)">
-      <div className="relative h-20 w-20">
-        <Image src={auctionImage!} alt={auction.title} fill className="mr-3 rounded-lg object-contain" />
-      </div>
-      <div className="ml-2 flex-1">
-        <div className="flex items-center justify-between">
-          <div>
-            <h3 className="mr-2 flex-1 font-medium text-[#1F1F25]">{auction.title}</h3>
-          </div>
-
-          <Badge
-            className={`${
-              auction.status === 'OPEN' && -24 < diffDay && diffDay < 0
-                ? 'bg-[#D84A5F] hover:bg-[#D84A5F]'
-                : 'bg-[#5B80C2] hover:bg-[#5B80C2]'
-            } px-2 py-1 font-normal text-white`}
-          >
-            {remainTime}
-          </Badge>
+    <li>
+      <Link
+        href={`/auctions/${auction.auction_id}`}
+        className="flex cursor-pointer items-center p-3 transition-colors hover:bg-(--color-secondary)"
+      >
+        <div className="relative h-20 w-20">
+          <Image src={auctionImage!} alt={auction.title} fill className="mr-3 rounded-lg object-contain" />
         </div>
-        <div className="mt-2 flex items-center gap-3 text-sm text-[#B8B8B8]">
-          <div className="flex items-center gap-3">
-            <i className="flex items-center gap-1">
-              <FaHeart className="mr-1 text-sm text-(--color-red)" />
-              <span>{favoritesCount}</span>
-            </i>
+        <div className="ml-2 flex-1">
+          <div className="flex items-center justify-between">
+            <div>
+              <h3 className="mr-2 flex-1 font-medium text-[#1F1F25]">{auction.title}</h3>
+            </div>
 
-            <i className="flex items-center gap-1">
-              <FaBookOpen className="text-sm text-(--color-primary)" />
-              <span>{episodesCount}</span>
-            </i>
+            <Badge
+              className={`${
+                auction.status === 'OPEN' && -24 < diffDay && diffDay < 0
+                  ? 'bg-[#D84A5F] hover:bg-[#D84A5F]'
+                  : 'bg-[#5B80C2] hover:bg-[#5B80C2]'
+              } px-2 py-1 font-normal text-white`}
+            >
+              {remainTime}
+            </Badge>
+          </div>
+          <div className="mt-2 flex items-center gap-3 text-sm text-[#B8B8B8]">
+            <div className="flex items-center gap-3">
+              <i className="flex items-center gap-1">
+                <FaHeart className="mr-1 text-sm text-(--color-red)" />
+                <span>{favoritesCount}</span>
+              </i>
+
+              <i className="flex items-center gap-1">
+                <FaBookOpen className="text-sm text-(--color-primary)" />
+                <span>{episodesCount}</span>
+              </i>
+            </div>
           </div>
         </div>
-      </div>
+      </Link>
     </li>
   );
 };
--- apps/web/src/components/main/PopularAuctionCard.tsx
+++ apps/web/src/components/main/PopularAuctionCard.tsx
@@ -2,6 +2,8 @@ import { SortedAuctionItemType } from 'src/types/main';
 import NotAuctionImage from 'assets/images/auctionDefault.png';
 import Image from 'next/image';
 import { FaBookOpen, FaHeart } from 'react-icons/fa6';
+import Link from 'next/link';
+import testImage from 'assets/images/test.png';
 
 const PopularAuctionCard = ({ auction }: { auction: SortedAuctionItemType }) => {
   const isImage = auction.image_urls && auction.image_urls.length > 0;
@@ -11,26 +13,28 @@ const PopularAuctionCard = ({ auction }: { auction: SortedAuctionItemType }) => 
   const episodesCount = auction.episodes && auction.episodes.length > 0 ? auction.episodes.length : 0;
 
   return (
-    <li className="relative cursor-pointer overflow-hidden rounded-lg transition-transform hover:scale-[1.02]">
-      <div className="relative h-[200px] w-full">
-        <Image src={auctionImage!} alt={auction.title} fill className="rounded-lg object-cover p-1" />
+    <li className="transition-transform hover:scale-[1.02]">
+      <Link href={`/auctions/${auction.auction_id}`} className="relative overflow-hidden rounded-lg">
+        <div className="relative h-[200px] w-full">
+          <Image src={testImage!} alt={auction.title} fill className="rounded-lg object-cover p-1" />
 
-        <div className="absolute bottom-0 left-0 p-3 text-white">
-          <h3 className="line-clamp-2 text-sm font-medium">{auction.title}</h3>
-          <address className="text-xs">{auction.address[0]}</address>
-          <div className="mt-2 flex items-center gap-3 text-xs">
-            <i className="flex items-center gap-1">
-              <FaHeart className="mr-1 text-sm text-(--color-red)" />
-              <span>{favoritesCount}</span>
-            </i>
+          <div className="absolute bottom-0 left-0 p-3 text-white">
+            <h3 className="line-clamp-2 text-sm font-medium">{auction.title}</h3>
+            <address className="text-xs">{auction.address[0]}</address>
+            <div className="mt-2 flex items-center gap-3 text-xs">
+              <i className="flex items-center gap-1">
+                <FaHeart className="mr-1 text-sm text-(--color-red)" />
+                <span>{favoritesCount}</span>
+              </i>
 
-            <i className="flex items-center gap-1">
-              <FaBookOpen className="text-sm text-(--color-primary)" />
-              <span>{episodesCount}</span>
-            </i>
+              <i className="flex items-center gap-1">
+                <FaBookOpen className="text-sm text-(--color-primary)" />
+                <span>{episodesCount}</span>
+              </i>
+            </div>
           </div>
         </div>
-      </div>
+      </Link>
     </li>
   );
 };
