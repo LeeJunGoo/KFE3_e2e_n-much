28421d6b377dd8d68163ab4c339da0c89804026a
kimsh0306 <b666790@gmail.com>
2025-06-28 20:35:53 +0900

    chore: PageContainer 적용 (#62)

--- apps/web/src/app/auth/layout.tsx
+++ apps/web/src/app/auth/layout.tsx
@@ -1,9 +1,10 @@
 import React, { Suspense } from 'react';
+import PageContainer from 'src/components/layout/PageContainer';
 
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
-    <>
+    <PageContainer>
       <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>
-    </>
+    </PageContainer>
   );
 }
