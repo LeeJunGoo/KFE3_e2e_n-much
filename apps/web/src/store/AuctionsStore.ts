5e19d3970dd981d101e047289244a9664f45ce7c
kimsh0306 <b666790@gmail.com>
2025-06-28 19:42:52 +0900

    feat: store 생성 (#50)

---
+++ apps/web/src/store/AuctionsStore.ts
@@ -0,0 +1,13 @@
+import { create } from 'zustand';
+import { AuctionRow } from 'src/lib/supabase/type';
+
+interface AuctionsStore {
+  auctions: AuctionRow[];
+  setAuctions: (auctions: AuctionRow[]) => void;
+}
+
+export const useAuctionsStore = create<AuctionsStore>((set) => ({
+  // 키워드로 검색한 경매 리스트
+  auctions: [],
+  setAuctions: (newAuctions: AuctionRow[]) => set({ auctions: newAuctions })
+}));
