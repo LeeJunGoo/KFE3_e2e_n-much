b1f9888b79836835ea5dd27095c1f7484b4b1aad
kimsh0306 <b666790@gmail.com>
2025-06-28 19:52:01 +0900

    chore: 코드 정리 (#50)

--- apps/web/src/components/common/SearchBar.tsx
+++ apps/web/src/components/common/SearchBar.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import useRecentKeywords from 'src/hooks/useRecentKeywords';
 import usePopularKeywords from 'src/hooks/usePopularKeywords';
 import { useAuctionsStore } from 'src/store/AuctionsStore';
