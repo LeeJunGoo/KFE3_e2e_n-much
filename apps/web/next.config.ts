35987709496a93ef39df44f43d8433ed9c48052d
LeeJnuGoo <leejungoo1396@gmail.com>
2025-06-30 16:09:27 +0900

    [chore] remotePatterns 도메인 경로 추가 (#64)

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -5,10 +5,10 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       { protocol: 'https', hostname: 'api.dicebear.com' },
       { protocol: 'https', hostname: 'i.namu.wiki' },
-      { protocol: 'https', hostname: 'psszbhuartnhkzomgxmq.supabase.co' }
+      { protocol: 'https', hostname: 'psszbhuartnhkzomgxmq.supabase.co' },
+      { protocol: 'https', hostname: 'lh3.googleusercontent.com' }
     ]
   }
 };
 
 export default nextConfig;
-//FIXME - i.namu.wiki 삭제하기 (테스트용)
