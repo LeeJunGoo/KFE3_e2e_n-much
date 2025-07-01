ea2e8242d4bd83ab03d6e20ca423d26889836725
kimsh0306 <b666790@gmail.com>
2025-07-02 02:36:44 +0900

    feat: zustand의 persist 미들웨어 적용 (#70)

--- apps/web/src/store/UserStore.ts
+++ apps/web/src/store/UserStore.ts
@@ -1,4 +1,5 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 import type { BuyerUpdate, SellerUpdate } from 'src/lib/supabase/type';
 import type { Role } from '../types/auth/index';
 
@@ -10,8 +11,15 @@ interface UserStore {
   setUser: (userInfo: User, userRole: Role | null) => void;
 }
 
-export const useUserStore = create<UserStore>((set) => ({
-  userInfo: null,
-  userRole: null,
-  setUser: (newUserInfo: User, newUserRole: Role | null) => set({ userInfo: newUserInfo, userRole: newUserRole })
-}));
+export const useUserStore = create<UserStore>()(
+  persist(
+    (set) => ({
+      userInfo: null,
+      userRole: null,
+      setUser: (newUserInfo: User, newUserRole: Role | null) => set({ userInfo: newUserInfo, userRole: newUserRole })
+    }),
+    {
+      name: 'user-store' // localStorage에 저장될 key
+    }
+  )
+);
