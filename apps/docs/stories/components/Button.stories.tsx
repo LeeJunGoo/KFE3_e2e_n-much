9e851dd76e5da7772ea0d446f83b831ef55b24cc
Seoyoung Park <seokachuu@gmail.com>
2025-07-01 21:59:17 +0900

    feat: mypage episodes page suspense 추가 (#68)

---
+++ apps/docs/stories/components/Button.stories.tsx
@@ -0,0 +1,15 @@
+// import type { StoryObj, Meta } from '@storybook/nextjs';
+
+// import { button } from '@repo/ui/ui/src/components/ui/button';
+
+// export default {
+//   component: button.stories
+// } satisfies Meta<typeof button.stories>;
+
+// type Story = StoryObj<typeof button.stories>;
+
+// export const Default: Story = {
+//   args: {
+//     title: '버튼 테스트'
+//   }
+// };
