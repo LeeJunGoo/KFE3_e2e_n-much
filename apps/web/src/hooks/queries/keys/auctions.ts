eaaa906e878e857b5246cf7849366a43ded8d989
Seoyoung Park <seokachuu@gmail.com>
2025-07-03 23:55:02 +0900

    bugfix: 경매자가 올린 데이터 리스트 불러오기 수정 (#73)

--- apps/web/src/hooks/queries/keys/auctions.ts
+++ apps/web/src/hooks/queries/keys/auctions.ts
@@ -3,5 +3,6 @@ export const auctionQueryKeys = {
   detail: (auctionId: string) => [...auctionQueryKeys.all, 'detail', auctionId] as const,
   created: (creatorId: string) => [...auctionQueryKeys.all, 'created', creatorId] as const,
   bid: (bidderId: string) => [...auctionQueryKeys.all, 'bid', bidderId] as const,
-  sellerAuctions: () => [...auctionQueryKeys.all, 'sellerAuctions'] as const
+  sellerAuctions: () => [...auctionQueryKeys.all, 'sellerAuctions'] as const,
+  sellerMypageAuctions: (sellerId: string) => ['myPage', 'sellerAuctions', sellerId] as const
 };
