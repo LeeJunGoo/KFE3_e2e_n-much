19ee5d3f78ddc675d7d14d7ad3440c720b49c288
LeeJnuGoo <leejungoo1396@gmail.com>
2025-07-04 20:42:44 +0900

    bugfix: 외부 도메인 추가

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -2,7 +2,11 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [{ protocol: 'https', hostname: 'psszbhuartnhkzomgxmq.supabase.co' }]
+    remotePatterns: [
+      { protocol: 'https', hostname: 'psszbhuartnhkzomgxmq.supabase.co' },
+      { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
+      { protocol: 'http', hostname: 'img1.kakaocdn.net' }
+    ]
   }
 };
 
