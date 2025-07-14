d0acf93f3b96cd35d52b9a29f7990269a4ea4cc4
baram55 <asd7973@gmail.com>
2025-07-15 02:08:55 +0900

    refactor: 쿼리 스트링이 없을 경우, 경매 데이터를 가져오는 기능 제거 (#118)

--- apps/web/src/app/(with-global-layout)/auctions/write/page.tsx
+++ apps/web/src/app/(with-global-layout)/auctions/write/page.tsx
@@ -1,3 +1,7 @@
+//NOTE - searchParams 카멜 케이스 구조 분해 할당 카멜 케이스로 나옴
+//NOTE - auction_id 빈 값일 경우 null로 처리
+//NOTE - PageHeader 사라짐
+
 import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
 import { fetchAuctionById } from 'src/entities/auction/api';
 import AuctionForm from 'src/features/auction/AuctionForm';
@@ -5,19 +9,24 @@ import AuctionForm from 'src/features/auction/AuctionForm';
 
 const AuctionWritePage = async ({ searchParams }: { searchParams: Promise<{ [key: string]: string | undefined }> }) => {
   const { auction_id } = await searchParams;
-  const queryClient = new QueryClient();
 
-  await queryClient.prefetchQuery({
-    queryKey: ['auctionForm'],
-    queryFn: () => fetchAuctionById(auction_id)
-  });
+  if (!auction_id) {
+    return <AuctionForm auctionIdParam={undefined} />;
+  } else {
+    const queryClient = new QueryClient();
 
-  return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      {/* <PageHeader>{auction_id ? '경매 수정' : '경매 등록'}</PageHeader> */}
-      <AuctionForm auctionIdParam={auction_id} />
-    </HydrationBoundary>
-  );
+    await queryClient.prefetchQuery({
+      queryKey: ['auctionForm'],
+      queryFn: () => fetchAuctionById(auction_id)
+    });
+
+    return (
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        {/* <PageHeader>{auction_id ? '경매 수정' : '경매 등록'}</PageHeader> */}
+        <AuctionForm auctionIdParam={auction_id} />
+      </HydrationBoundary>
+    );
+  }
 };
 
 export default AuctionWritePage;
