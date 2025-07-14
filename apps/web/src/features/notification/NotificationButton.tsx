0a6d3a82c3e05809582301e392402f8348f1ed65
Seoyoung Park <seokachuu@gmail.com>
2025-07-14 17:53:48 +0900

    fix: 병합 완료

--- apps/web/src/features/notification/NotificationButton.tsx
+++ apps/web/src/features/notification/NotificationButton.tsx
@@ -1,13 +1,26 @@
+'use client';
+import { useState } from 'react';
 import { Button } from '@repo/ui/components/ui/button';
 import { IoNotifications } from 'react-icons/io5';
+import NotificationPopover from 'src/features/notification/NotificationPopover';
 
 const NotificationButton = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  //NOTE - 임시적으로 알림이 없다고 가정
+  const hasUnread = false;
   return (
     <div className="flex items-center">
-      <Button variant="text" className="relative !p-0">
-        <IoNotifications className="text-(--color-accent) size-6" />
-        <span className="bg-(--color-red) absolute right-0.5 top-1.5 size-2 rounded-full" />
-      </Button>
+      <NotificationPopover
+        isOpen={isOpen}
+        onOpenChange={setIsOpen}
+        trigger={
+          <Button variant="text" className="relative !p-0">
+            <IoNotifications className="text-(--color-accent) size-6" />
+            {hasUnread && <span className="bg-(--color-red) absolute right-0.5 top-1.5 size-2 rounded-full" />}
+          </Button>
+        }
+      />
     </div>
   );
 };
