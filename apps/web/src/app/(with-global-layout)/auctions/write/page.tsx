658c50c971e9a69ff06c5346f81e317c6ab383cd
baram55 <asd7973@gmail.com>
2025-07-16 20:07:52 +0900

    refactor: ActionWritePage 컴포넌트의 이름을 AuctionFormPage로 수정 (#118)

--- apps/web/src/app/(with-global-layout)/auctions/write/page.tsx
+++ apps/web/src/app/(with-global-layout)/auctions/write/page.tsx
@@ -1,29 +1,33 @@
-//NOTE - 서영님 PageHeader import해서 사용하기
-
 import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
 import { getAuctionWIthAddress } from 'src/entities/auction/api';
 import AuctionForm from 'src/features/auction/AuctionForm';
-// import PageHeader from 'src/widgets/PageHeader';
+import DetailPageHeader from 'src/widgets/DetailPageHeader';
 
-const AuctionWritePage = async ({ searchParams }: { searchParams: Promise<{ [key: string]: string | undefined }> }) => {
+const AuctionFormPage = async ({ searchParams }: { searchParams: Promise<{ [key: string]: string | undefined }> }) => {
   const { auction_id: auctionId } = await searchParams;
+
   if (!auctionId) {
-    return <AuctionForm auctionIdParam={null} />;
-  } else {
-    const queryClient = new QueryClient();
-
-    await queryClient.prefetchQuery({
-      queryKey: ['auctionForm'],
-      queryFn: () => getAuctionWIthAddress(auctionId)
-    });
-
     return (
-      <HydrationBoundary state={dehydrate(queryClient)}>
-        {/* <PageHeader>{auction_id ? '경매 수정' : '경매 등록'}</PageHeader> */}
-        <AuctionForm auctionIdParam={auctionId} />
-      </HydrationBoundary>
+      <>
+        <DetailPageHeader>{'경매 등록'}</DetailPageHeader>
+        <AuctionForm auctionIdParam={null} />
+      </>
     );
   }
+
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ['auctionForm'],
+    queryFn: () => getAuctionWIthAddress(auctionId)
+  });
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <DetailPageHeader>{'경매 등록'}</DetailPageHeader>
+      <AuctionForm auctionIdParam={auctionId} />
+    </HydrationBoundary>
+  );
 };
 
-export default AuctionWritePage;
+export default AuctionFormPage;
