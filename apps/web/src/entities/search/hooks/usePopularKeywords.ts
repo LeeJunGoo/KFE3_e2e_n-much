6b3c55b110e451b3dcce8c396f97721d77287876
kimsh0306 <b666790@gmail.com>
2025-07-18 10:34:14 +0900

    feat: tanstack 쿼리 적용 (#114)

--- apps/web/src/entities/search/hooks/usePopularKeywords.ts
+++ apps/web/src/entities/search/hooks/usePopularKeywords.ts
@@ -1,29 +1,24 @@
 'use client';
 
-import { useState, useEffect } from 'react';
+import { useQuery } from '@tanstack/react-query';
 import { getPopularKeywords } from 'src/entities/search/api';
 
 const usePopularKeywords = () => {
-  const [popularKeywords, setPopularKeywords] = useState<string[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
+  const {
+    data: popularKeywords,
+    isLoading,
+    isError,
+    error
+  } = useQuery({
+    queryKey: ['popularKeywords'],
+    queryFn: getPopularKeywords
+  });
 
-  useEffect(() => {
-    const fetchKeywords = async () => {
-      setIsLoading(true);
-      try {
-        const data = await getPopularKeywords();
-        setPopularKeywords(data);
-      } catch (error) {
-        console.error('인기 검색어 로딩 실패:', error);
-      } finally {
-        setIsLoading(false);
-      }
-    };
+  if (isError) {
+    console.error('인기 검색어 로딩 실패:', error);
+  }
 
-    fetchKeywords();
-  }, []);
-
-  return { popularKeywords, isLoading };
+  return { popularKeywords: popularKeywords ?? [], isLoading };
 };
 
 export default usePopularKeywords;
