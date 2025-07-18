1f21c3adeaf74964819c903fd735fcf71ae6cb8b
kimsh0306 <b666790@gmail.com>
2025-07-18 10:33:45 +0900

    feat: 스켈레톤 적용 (#114)

--- apps/web/src/features/search/components/PopularKeywords.tsx
+++ apps/web/src/features/search/components/PopularKeywords.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { LoadingSpinner } from 'src/shared/ui/LoadingSpinner';
+import PopularKeywordsSkeleton from './PopularKeywordsSkeleton';
 
 interface PopularKeywordsProps {
   keywords: string[];
@@ -8,43 +8,54 @@ interface PopularKeywordsProps {
   isLoading?: boolean;
 }
 
-// 스켈레톤 적용
 const PopularKeywords = ({ keywords, handleKeywordClick, isLoading }: PopularKeywordsProps) => {
   const LEFT_SLICE_COUNT = { start: 0, end: 5 };
   const RIGHT_SLICE_COUNT = { start: 5, end: 10 };
-
-  const getKeywordItem = (keyword: string, idx: number) => {
-    return (
-      <div
-        key={`popular-${keyword}`}
-        className="flex cursor-pointer items-center justify-between"
-        onClick={() => handleKeywordClick(keyword)}
-      >
-        <div className="flex items-center">
-          <span className="text-(--color-accent) w-6 font-medium">{idx + 1}</span>
-          <span className={`truncate text-sm ${isLoading && 'text-(--color-light-gray)'}`}>{keyword}</span>
-        </div>
-      </div>
-    );
-  };
+  const LEFT_RANK_START = 1;
+  const RIGHT_RANK_START = LEFT_SLICE_COUNT.end;
 
   return (
     <div>
       <div className="mb-3 flex items-center">
         <h3>인기 검색어</h3>
-        {isLoading && <LoadingSpinner size="sm" className="ml-2" />}
       </div>
       <div className="grid grid-cols-2 gap-2">
-        <div className="flex w-full flex-col gap-2">
-          {keywords
-            .slice(LEFT_SLICE_COUNT.start, LEFT_SLICE_COUNT.end)
-            .map((keyword, idx) => getKeywordItem(keyword, idx))}
-        </div>
-        <div className="flex w-full flex-col gap-2">
-          {keywords
-            .slice(RIGHT_SLICE_COUNT.start, RIGHT_SLICE_COUNT.end)
-            .map((keyword, idx) => getKeywordItem(keyword, idx + LEFT_SLICE_COUNT.end))}
-        </div>
+        {isLoading ? (
+          Array.from({ length: 10 }, (_, index) => index).map((index) => (
+            <PopularKeywordsSkeleton className="h-6 w-full" key={`popular-${index}`} />
+          ))
+        ) : (
+          <>
+            <div className="flex w-full flex-col gap-2">
+              {keywords.slice(LEFT_SLICE_COUNT.start, LEFT_SLICE_COUNT.end).map((keyword, idx) => (
+                <div
+                  key={`popular-${keyword}`}
+                  className="flex cursor-pointer items-center justify-between"
+                  onClick={() => handleKeywordClick(keyword)}
+                >
+                  <div className="flex items-center">
+                    <span className="text-(--color-accent) size-6 font-medium">{idx + LEFT_RANK_START}</span>
+                    <span className={`truncate text-sm ${isLoading && 'text-(--color-light-gray)'}`}>{keyword}</span>
+                  </div>
+                </div>
+              ))}
+            </div>
+            <div className="flex w-full flex-col gap-2">
+              {keywords.slice(RIGHT_SLICE_COUNT.start, RIGHT_SLICE_COUNT.end).map((keyword, idx) => (
+                <div
+                  key={`popular-${keyword}`}
+                  className="flex cursor-pointer items-center justify-between"
+                  onClick={() => handleKeywordClick(keyword)}
+                >
+                  <div className="flex items-center">
+                    <span className="text-(--color-accent) size-6 font-medium">{idx + RIGHT_RANK_START}</span>
+                    <span className={`truncate text-sm ${isLoading && 'text-(--color-light-gray)'}`}>{keyword}</span>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
