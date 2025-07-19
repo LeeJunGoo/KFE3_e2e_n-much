d1387fe53ae5ff0086fb8c325152ea91a9a3794d
AhnJUWon <urams79@gmail.com>
2025-07-19 19:09:46 +0900

    remove : mascot image-(#127)

--- apps/web/src/entities/layout/metaData.ts
+++ apps/web/src/entities/layout/metaData.ts
@@ -8,7 +8,7 @@ export const home = {
     description: '당신의 가치를 입찰하세요!',
     images: [
       {
-        url: 'https://pukmjrqqelymnkzflppa.supabase.co/storage/v1/object/public/opengraph//mascot_opengraph.png',
+        url: 'https://pukmjrqqelymnkzflppa.supabase.co/storage/v1/object/public/opengraph//mascot_opengraph.webp',
         width: 1200,
         height: 630,
         alt: 'Vidding'
