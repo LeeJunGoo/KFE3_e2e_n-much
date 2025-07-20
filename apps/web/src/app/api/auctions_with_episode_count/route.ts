6e57a85c42bc444d49658707c38cf7ee203cb7b5
baram55 <asd7973@gmail.com>
2025-07-21 06:24:07 +0900

    refactor: 라우트 핸들러 컨벤션에 맞게 수정 (#118)

--- apps/web/src/app/api/auctions_with_episode_count/route.ts
+++ apps/web/src/app/api/auctions_with_episode_count/route.ts
@@ -1,26 +1,30 @@
 import { NextResponse } from 'next/server';
 import { getAllAuctionsWithEpisodeCountByOrder } from 'src/entities/auction/supabase';
-import type { NextRequest} from 'next/server';
+import type { NextRequest } from 'next/server';
 
 //TODO - 팀원들과 정렬순을 의논해서 수정할 것
 export async function GET(request: NextRequest) {
-  const {searchParams} = request.nextUrl;
-  const orderParam = searchParams.get('order');
-  const pageParam = searchParams.get('page');
+  const { searchParams } = request.nextUrl;
+  const order = searchParams.get('order');
+  const page = searchParams.get('page');
 
+  if (!order || !page) {
+    return NextResponse.json({ error: '400: 필수 값이 존재하지 않습니다.' }, { status: 400 });
+  }
   try {
-    if (orderParam === 'favorites') {
-      const res = await getAllAuctionsWithEpisodeCountByOrder(orderParam, false, Number(pageParam));
-      return NextResponse.json({ status: 'success', data: res });
-    } else if (orderParam === 'end_time') {
-      const res = await getAllAuctionsWithEpisodeCountByOrder(orderParam, true, Number(pageParam));
-      return NextResponse.json({ status: 'success', data: res });
-    } else if (orderParam === 'created_at') {
-      const res = await getAllAuctionsWithEpisodeCountByOrder(orderParam, true, Number(pageParam));
-      return NextResponse.json({ status: 'success', data: res });
+    if (order === 'favorites') {
+      const res = await getAllAuctionsWithEpisodeCountByOrder(order, false, Number(page));
+      return NextResponse.json(res, { status: 200 });
+    } else if (order === 'end_time') {
+      const res = await getAllAuctionsWithEpisodeCountByOrder(order, true, Number(page));
+      return NextResponse.json(res, { status: 200 });
+    } else if (order === 'created_at') {
+      const res = await getAllAuctionsWithEpisodeCountByOrder(order, true, Number(page));
+      return NextResponse.json(res, { status: 200 });
     }
-    throw new Error('경매와 사연 갯수 불러오기 에러(잘못된 정렬 순서)');
   } catch (error) {
-    return NextResponse.json({ status: 'error', error: `Server Error${  error}` }, { status: 500 });
+    if (error instanceof Error) {
+      return NextResponse.json({ error: '500: 서버 처리 중 오류가 발생했습니다.' }, { status: 500 });
+    }
   }
 }
