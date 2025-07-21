82319d527ca220ac09327aab7058a7b6d4faf654
baram55 <asd7973@gmail.com>
2025-07-21 16:42:56 +0900

    refactor: url을 auction_card_list로 수정 (#137)

--- apps/web/src/entities/auction/api.ts
+++ apps/web/src/entities/auction/api.ts
@@ -104,7 +104,7 @@ export const getAuctionCardList = async ({
   }
 
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/auctions_with_episode_count?order=${order}&page=${pageParam}`
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/auction_card_list?order=${order}&page=${pageParam}`
   );
   if (!res.ok) {
     throw new Error('모든 경매와 해당 경매의 사연 갯수 fetch 실패');
