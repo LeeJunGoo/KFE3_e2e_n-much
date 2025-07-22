3a88d2b9d58a5a47386a97a5d4c31227a5195421
baram55 <asd7973@gmail.com>
2025-07-23 03:31:03 +0900

    feat: 로그인한 유저가 경매 작성자가 아닌 경우, 수정 페이지에서 메인 페이지로 리다이렉트하는 기능 추가 (#147)

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import { getServerUser } from 'src/entities/auth/serverAction';
+import { getServerUser, selectUserIdByAuctionId } from 'src/entities/auth/serverAction';
 import type { NextRequest } from 'next/server';
 
 export const middleware = async (request: NextRequest) => {
@@ -14,11 +14,23 @@ export const middleware = async (request: NextRequest) => {
     }
   }
 
-  //NOTE - 경매 수정페이지에서 로그인되어 있지 않으면 로그인 페이지로 이동
+  //NOTE - 경매 수정 페이지에서 로그인되어 있지 않으면 로그인 페이지로 이동
   if (pathName === '/auctions/write' && searchParams.get('auction_id')) {
     const userInfo = await getServerUser();
     if (!userInfo) {
       return NextResponse.redirect(new URL('/auth/signup', request.url));
+    }
+    //NOTE - 경매 수정 페이지의 작성자가 로그인한 유저가 아니면 메인 페이지로 이동
+    try {
+      const userId = userInfo.id;
+      const auctionId = searchParams.get('auction_id');
+      const authorId: string = await selectUserIdByAuctionId(auctionId);
+
+      if (authorId !== userId) {
+        return NextResponse.redirect(new URL('/main', request.url));
+      }
+    } catch (error) {
+      return NextResponse.redirect(new URL('/main', request.url));
     }
   }
 
