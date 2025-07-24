21fffa656c5464dd11fc1ec3600f77b73a2bdcd4
baram55 <asd7973@gmail.com>
2025-07-24 09:04:20 +0900

    feat: appId 키 등록 (#153)

--- apps/web/src/app/(with-local-layout)/(with-header-layout)/push_sample/page.tsx
+++ apps/web/src/app/(with-local-layout)/(with-header-layout)/push_sample/page.tsx
@@ -1,4 +1,13 @@
+import { useEffect } from 'react';
+import OneSignal from 'react-onesignal';
+
 const page = () => {
+  useEffect(() => {
+    OneSignal.init({
+      appId: process.env.NEXT_PUBLIC_APP_ID!,
+      safari_web_id: process.env.NEXT_PUBLIC_SAFARI_WEB_ID!
+    });
+  }, []);
   return <>sample</>;
 };
 
