69f29279a67f9d6791b7f3db21d80f175071dde6
baram55 <asd7973@gmail.com>
2025-07-24 09:05:15 +0900

    feat: 개발자 모드 설정 (#153)

--- apps/web/src/app/(with-local-layout)/(with-header-layout)/push_sample/page.tsx
+++ apps/web/src/app/(with-local-layout)/(with-header-layout)/push_sample/page.tsx
@@ -5,7 +5,8 @@ const page = () => {
   useEffect(() => {
     OneSignal.init({
       appId: process.env.NEXT_PUBLIC_APP_ID!,
-      safari_web_id: process.env.NEXT_PUBLIC_SAFARI_WEB_ID!
+      safari_web_id: process.env.NEXT_PUBLIC_SAFARI_WEB_ID!,
+      allowLocalhostAsSecureOrigin: true //NOTE - 개발자 모드(localhost 사용 가능)
     });
   }, []);
   return <>sample</>;
