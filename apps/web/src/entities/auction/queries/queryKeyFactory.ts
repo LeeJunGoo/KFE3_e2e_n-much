c1bff077c6b0cb6be8c37db6f894f1a713a33bab
baram55 <asd7973@gmail.com>
2025-07-25 07:21:39 +0900

    refactor: query key에서 item의 매개 변수가 undefined 인 경우 제거 (#155)

--- apps/web/src/entities/auction/queries/queryKeyFactory.ts
+++ apps/web/src/entities/auction/queries/queryKeyFactory.ts
@@ -3,15 +3,13 @@ import { ADDRESS_ID_QUERY_KEY, AUCTION_FORM_QUERY_KEY, AUCTION_LIST_QUERY_KEY } 
 //NOTE - auctionForm에 들어갈 데이터를 가져오는데 사용
 export const auctionFormKeys = {
   all: [AUCTION_FORM_QUERY_KEY] as const,
-  item: (auctionId: string | undefined) =>
-    auctionId ? ([...auctionFormKeys.all, auctionId] as const) : ([...auctionFormKeys.all] as const)
+  item: (auctionId: string) => [...auctionFormKeys.all, auctionId] as const
 };
 
 //NOTE - addressId를 가져올 때 사용
 export const addressIdKeys = {
   all: [ADDRESS_ID_QUERY_KEY] as const,
-  item: (userId: string | undefined) =>
-    userId ? ([...addressIdKeys.all, userId] as const) : ([...addressIdKeys.all] as const)
+  item: (userId: string) => [...addressIdKeys.all, userId] as const
 };
 
 //NOTE - auctionList를 가져올 때 사용
