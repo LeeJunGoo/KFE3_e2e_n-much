ce526a658bb6d8160aff463448122dfc8b12ec70
AhnJUWon <urams79@gmail.com>
2025-07-25 17:25:26 +0900

    merge : 병합완료-(#132)

--- apps/web/src/features/addresses/components/AddressForm.tsx
+++ apps/web/src/features/addresses/components/AddressForm.tsx
@@ -1,12 +1,40 @@
 'use client';
 
-import { useState } from 'react';
 import { Button } from '@repo/ui/components/ui/button';
-import { Dialog, DialogContent, DialogHeader, DialogTitle } from '@repo/ui/components/ui/dialog';
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from '@repo/ui/components/ui/dialog';
 import { Input } from '@repo/ui/components/ui/input';
+import { useState } from 'react';
+import { DaumPostcodeEmbed } from 'react-daum-postcode';
 
 const AddressForm = () => {
-  const [daumPostCodeModal, setDaumPostCodeModal] = useState<boolean>(false);
+  const [zonecode, setZonecode] = useState('');
+  const [address, setAddress] = useState('');
+
+  const handleComplete = (data: {
+    address: string;
+    addressType: string;
+    bname: string;
+    buildingName: string;
+    zonecode: string;
+  }) => {
+    let fullAddress = data.address;
+    let extraAddress = '';
+
+    if (data.addressType === 'R') {
+      if (data.bname !== '') {
+        extraAddress += data.bname;
+      }
+      if (data.buildingName !== '') {
+        extraAddress += extraAddress !== '' ? `, ${data.buildingName}` : data.buildingName;
+      }
+      fullAddress += extraAddress !== '' ? ` (${extraAddress})` : '';
+    }
+
+    setZonecode(data.zonecode);
+    setAddress(fullAddress);
+
+    console.log(fullAddress); // e.g. '서울 성동구 왕십리로2길 20 (성수동1가)'
+  };
 
   return (
     <form>
@@ -23,23 +51,25 @@ const AddressForm = () => {
           <span className="text-(--color-red) translate-y-0.5">*</span>
         </label>
         <div className="mb-2 flex items-stretch gap-2">
-          <Input type="text" placeholder="우편번호" maxLength={10} readOnly />
-          <Button onClick={() => setDaumPostCodeModal(true)} variant="active" className="h-auto px-10">
-            주소 검색
-          </Button>
-          <Dialog open={daumPostCodeModal} onOpenChange={setDaumPostCodeModal}>
+          <Input type="text" placeholder="우편번호" maxLength={10} readOnly value={zonecode} />
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button variant="active" className="h-auto px-10">
+                주소 검색
+              </Button>
+            </DialogTrigger>
             <DialogContent className="sm:max-w-[425px]" aria-describedby={undefined}>
               <DialogHeader>
                 <DialogTitle className="mb-4 text-center text-lg font-bold">{'주소검색'}</DialogTitle>
               </DialogHeader>
-              <div>아무거나</div>
+              <DaumPostcodeEmbed onComplete={handleComplete} />;
             </DialogContent>
           </Dialog>
         </div>
-        <Input type="text" placeholder="기본 주소" className="mb-2" maxLength={50} readOnly />
+        <Input type="text" placeholder="기본 주소" className="mb-2" maxLength={50} readOnly value={address} />
         <Input type="text" placeholder="나머지 주소 (선택 입력 가능)" maxLength={50} />
       </div>
-      <div className="w-ful absolute bottom-0 left-0 right-0 bg-white p-4">
+      <div className="absolute bottom-0 left-0 right-0 w-full bg-white p-4">
         <Button variant="base" className="w-full">
           등록하기
         </Button>
