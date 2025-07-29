80d19fdb2ecfd97342cd0a8c1408f9dadcec5604
LeeJnuGoo <leejungoo1396@gmail.com>
2025-07-29 20:15:56 +0900

    refactor: EmptyState 공통 컴포넌트 수정 (#172)

--- apps/web/src/features/layout/main/MainPage.tsx
+++ apps/web/src/features/layout/main/MainPage.tsx
@@ -4,6 +4,7 @@ import LatestListSection from 'src/features/auction/LatestListSection';
 import PopularListSection from 'src/features/auction/PopularListSection';
 import ContentEmpty from 'src/features/auction/shared/ContentEmpty';
 import MainBanner from 'src/features/layout/main/components/MainBanner';
+import EmptyState from 'src/features/user/mypage/components/shared/EmptyState';
 import AuctionErrorBoundary from 'src/shared/ui/AuctionErrorBoundary';
 import PageContainer from 'src/shared/ui/PageContainer';
 import GoTopButton from 'src/shared/utils/goTopButton';
@@ -16,9 +17,10 @@ const MainPage = async () => {
         {/* Ending Soon Auctions */}
         <AuctionErrorBoundary
           fallback={
-            <ContentEmpty
-              titleLabel="곧 종료되는 경매 물품을 불러오는 중 문제가 발생했어요."
-              contentLabel="잠시 후 다시 시도해주세요!"
+            <EmptyState
+              className="bg-(--color-background) rounded-lg border py-16 shadow-md"
+              title="곧 종료되는 경매 물품을 불러오는 중 문제가 발생했어요."
+              description="잠시 후 다시 시도해주세요!"
             />
           }
         >
