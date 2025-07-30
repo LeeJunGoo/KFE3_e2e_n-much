bdacd79c76c76a631ba7334bffd643861e2b3be1
baram55 <asd7973@gmail.com>
2025-07-30 20:42:59 +0900

    style: 경매 현황에서 검색 결과가 없을 경우, 에러 UI 대신 검색 결과가 없다는 UI를 출력하도록 수정 (#176)

--- apps/web/src/features/auction/list/page/AuctionListPage.tsx
+++ apps/web/src/features/auction/list/page/AuctionListPage.tsx
@@ -4,7 +4,7 @@ import { getAuctionCount } from 'src/entities/auction/serverActions';
 import WriteAuctionButton from 'src/features/auction/button/WriteAuctionButton';
 import AuctionList from 'src/features/auction/list/components/AuctionList';
 import SelectOrder from 'src/features/auction/list/components/SelectOrder';
-import ErrorState from 'src/features/user/mypage/components/shared/ErrorState';
+import EmptyState from 'src/features/user/mypage/components/shared/EmptyState';
 import PageContainer from 'src/shared/ui/PageContainer';
 import PageTitle from 'src/shared/ui/PageTitle';
 import GoTopButton from 'src/shared/utils/goTopButton';
@@ -18,18 +18,19 @@ const AuctionListPage = async ({ order, keyword }: AuctionListPageProps) => {
 
   return (
     <PageContainer>
-      <div className="mb-4 flex w-full flex-col md:flex-row md:items-center">
-        <PageTitle className="mb-4 md:mb-0">경매 리스트 </PageTitle>
-        <SelectOrder order={order} keyword={keyword} />
-      </div>
       {auctionCount ? (
-        <HydrationBoundary state={dehydrate(queryClient)}>
-          <AuctionList order={order} keyword={keyword} auctionCount={auctionCount} />
-        </HydrationBoundary>
+        <>
+          <div className="mb-4 flex w-full flex-col md:flex-row md:items-center">
+            <PageTitle className="mb-4 md:mb-0">경매 리스트 </PageTitle>
+            <SelectOrder order={order} keyword={keyword} />
+          </div>
+          <HydrationBoundary state={dehydrate(queryClient)}>
+            <AuctionList order={order} keyword={keyword} auctionCount={auctionCount} />
+          </HydrationBoundary>
+        </>
       ) : (
-        <ErrorState />
+        <EmptyState title={'검색 결과가 없습니다.'} className="min-h-[calc(100vh-4rem)] justify-center pb-16" />
       )}
-
       <WriteAuctionButton />
       <GoTopButton />
     </PageContainer>
