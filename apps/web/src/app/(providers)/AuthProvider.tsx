28590f4e5852293aee9b71d6cb703b17cea40c62
Seoyoung Park <seokachuu@gmail.com>
2025-07-30 20:38:44 +0900

    bugfix: 마이페이지 로그인 진입 시 유저 프로필 사라지는 현상 수정 (#164)

--- apps/web/src/app/(providers)/AuthProvider.tsx
+++ apps/web/src/app/(providers)/AuthProvider.tsx
@@ -1,27 +1,32 @@
 'use client';
 import type { ReactNode } from 'react';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { useAuthActions, useUserState } from 'src/entities/auth/stores/useAuthStore';
 import { createClient } from 'src/shared/supabase/client/client';
-import type { User } from '@supabase/supabase-js';
+import type { AuthChangeEvent, User, Session } from '@supabase/supabase-js';
 
 const AuthProvider = ({ user: initialUser, children }: { user: User | null; children: ReactNode }) => {
   const currentUser = useUserState();
   const { setUser, setLoading } = useAuthActions();
   const supabase = createClient();
+  const initialAuthRef = useRef(false);
 
   useEffect(() => {
-    if (initialUser && (!currentUser || currentUser.id !== initialUser.id)) {
-      setUser(initialUser);
-      setLoading(false);
-    } else if (!initialUser && !currentUser) {
-      setLoading(false);
-    }
-
     const {
       data: { subscription }
-    } = supabase.auth.onAuthStateChange((event: any, session: any) => {
+    } = supabase.auth.onAuthStateChange((event: AuthChangeEvent, session: Session | null) => {
+      if (!initialAuthRef.current && event === 'SIGNED_IN') {
+        initialAuthRef.current = true;
+        return;
+      }
       const sessionUser = session?.user ?? null;
+
+      if (initialUser && (!currentUser || currentUser.id !== initialUser.id)) {
+        setUser(initialUser);
+        setLoading(false);
+      } else if (!initialUser && !currentUser) {
+        setLoading(false);
+      }
 
       if (event === 'SIGNED_IN' && sessionUser) {
         setUser(sessionUser);
