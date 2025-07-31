e4f23615536d0639adc8016a03c0bb29a8e830b6
Seoyoung Park <seokachuu@gmail.com>
2025-07-31 16:11:08 +0900

    feat: 경매진행자 Mode 테마 추가 및 변경 (#185)

--- apps/web/src/widgets/layout/Header.tsx
+++ apps/web/src/widgets/layout/Header.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import Logo from 'src/assets/images/logo.svg';
+import LogoDark from 'src/assets/images/logo_dark.svg';
 import RoleSwitch from 'src/features/layout/header/RoleSwitch';
 import NotificationButton from 'src/features/notification/components/NotificationButton';
 
@@ -10,7 +11,8 @@ const Header = () => {
       <div className="flex h-full items-center justify-between">
         <Link href="/main">
           <h1 className="text-lg font-bold">
-            <Image src={Logo} alt="logo" className="size-12" />
+            <Image src={Logo} alt="logo" className="block size-12 dark:hidden" />
+            <Image src={LogoDark} alt="logo" className="hidden size-12 dark:block" />
           </h1>
         </Link>
         <div className="flex items-center">
