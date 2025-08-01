54bd7f7e7f5e30e28418798a0520541fd3e09590
Seoyoung Park <seokachuu@gmail.com>
2025-08-01 19:45:13 +0900

    refactor: 에피소드 페이지 쿼리스트링 추가 (#185)

--- apps/web/src/features/user/mypage/components/episodes/components/OngoingEpisodesContainer.tsx
+++ apps/web/src/features/user/mypage/components/episodes/components/OngoingEpisodesContainer.tsx
@@ -2,7 +2,7 @@ import MyEpisodeListItem from 'src/features/user/mypage/components/episodes/comp
 import EmptyState from 'src/shared/ui/EmptyState';
 import type { EpisodesContainerProps } from 'src/entities/user/mypage/episodes/types';
 
-const OngoingEpisodesContainer = ({ episodes }: EpisodesContainerProps) => {
+const OngoingEpisodesContainer = ({ episodes, currentTab }: EpisodesContainerProps) => {
   if (!episodes || episodes.length === 0) {
     return (
       <EmptyState title="진행중인 에피소드가 없습니다" description="새로운 경매에 참여해보세요" className="mt-24" />
@@ -12,7 +12,7 @@ const OngoingEpisodesContainer = ({ episodes }: EpisodesContainerProps) => {
   return (
     <ul>
       {episodes.map((episode) => (
-        <MyEpisodeListItem key={episode.episode_id} episode={episode} />
+        <MyEpisodeListItem key={episode.episode_id} episode={episode} currentTab={currentTab} />
       ))}
     </ul>
   );
