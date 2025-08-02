53bc81777fb7a8de89ae455522fa0c305b04cf62
LeeJnuGoo <leejungoo1396@gmail.com>
2025-08-02 20:30:12 +0900

    style: BaseAvatar 상수 위치 변경 (#188)

--- apps/web/src/shared/ui/BaseAvatar.tsx
+++ apps/web/src/shared/ui/BaseAvatar.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Avatar, AvatarFallback, AvatarImage } from '@repo/ui/components/ui/avatar';
 import Image from 'next/image';
 import DefaultAvatar from 'src/assets/images/avatarDefault.svg';
@@ -11,14 +10,14 @@ type BaseAvatarProps = {
   className?: string;
 };
 
-const BaseAvatar = ({ src, alt, size, className }: BaseAvatarProps) => {
-  const sizes = {
-    sm: 'size-8',
-    md: 'size-10',
-    lg: 'size-12',
-    xl: 'size-14'
-  };
+const sizes = {
+  sm: 'size-8',
+  md: 'size-10',
+  lg: 'size-12',
+  xl: 'size-14'
+};
 
+const BaseAvatar = ({ src, alt, size, className }: BaseAvatarProps) => {
   return (
     <Avatar className={twMerge(sizes[size], className)}>
       <AvatarImage src={src} alt={alt} />
