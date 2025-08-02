20bd20d5be465e89381fa553623b2720be2337ba
kimsh0306 <b666790@gmail.com>
2025-08-02 22:46:12 +0900

    bugFix: 불필요한 css 제거 (#196)

--- apps/web/src/features/user/mypage/components/main/skeleton/AddressStatusSkeleton.tsx
+++ apps/web/src/features/user/mypage/components/main/skeleton/AddressStatusSkeleton.tsx
@@ -2,19 +2,19 @@ import { Skeleton } from '@repo/ui/components/ui/skeleton';
 
 const AddressStatusSkeleton = () => {
   return (
-    <div className="text-(--color-warm-gray) border-(--color-warm-gray)/30 mt-3 border-t py-4 text-sm">
-      <div className="flex items-start justify-between">
+    <div className="border-(--color-warm-gray)/30 mt-3 border-t py-4">
+      <div>
         <Skeleton className="mb-5 h-6 w-10" />
       </div>
-      <div className="flex items-start gap-2">
+      <div className="flex gap-2">
         <Skeleton className="h-10 w-10 rounded-full" />
         <div className="-translate-y-1">
-          <div className="mb-1 flex items-center gap-2">
+          <div className="mb-1">
             <Skeleton className="w-30 h-5" />
           </div>
-          <p className="flex flex-col gap-1 sm:flex-row sm:items-center">
+          <div>
             <Skeleton className="h-5 w-60" />
-          </p>
+          </div>
         </div>
       </div>
     </div>
