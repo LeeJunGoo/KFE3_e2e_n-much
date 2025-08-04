34b1827256793f2236931963b3a75779b983d862
LeeJnuGoo <leejungoo1396@gmail.com>
2025-08-04 15:55:52 +0900

    chore: openWindow URL 배포 서버로 변경 (#188)

--- apps/web/public/sw.js
+++ apps/web/public/sw.js
@@ -1,4 +1,5 @@
 //ANCHOR - 사용자 지정 이미지와 알림을 지원하며, 수신되는 푸시 이벤트와 알림 클릭을 처리
+
 // 사용자의 브라우저가 서버로부터 푸시 알림을 수신할 때 실행
 self.addEventListener('push', function (event) {
   if (event.data) {
@@ -25,5 +26,5 @@ self.addEventListener('notificationclick', function (event) {
   const data = event.notification.data;
 
   event.notification.close();
-  event.waitUntil(clients.openWindow('https://localhost:3001'));
+  event.waitUntil(clients.openWindow('https://kfe-3-e2e-n-much-web.vercel.app'));
 });
