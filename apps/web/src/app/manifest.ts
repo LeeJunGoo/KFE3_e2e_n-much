d61aaaef644b964c2c5906db3cd46940b4fe4b96
AhnJUWon <urams79@gmail.com>
2025-08-04 20:06:04 +0900

    refactor : 경로변경(#195)

--- apps/web/src/app/manifest.ts
+++ apps/web/src/app/manifest.ts
@@ -17,53 +17,53 @@ export default function manifest(): MetadataRoute.Manifest {
     dir: 'ltr',
     icons: [
       {
-        src: '/apps/web/public/icons/vid_icon_48x48.webp',
+        src: '/icons/vid_icon_48x48.webp',
         sizes: '48x48',
         type: 'image/webp'
       },
       {
-        src: '/apps/web/public/icons/vid_icon_72x72.webp',
+        src: '/icons/vid_icon_72x72.webp',
         sizes: '72x72',
         type: 'image/webp'
       },
       {
-        src: '/apps/web/public/icons/vid_icon_96x96.webp',
+        src: '/icons/vid_icon_96x96.webp',
         sizes: '96x96',
         type: 'image/webp'
       },
       {
-        src: '/apps/web/public/icons/vid_icon_128x128.webp',
+        src: '/icons/vid_icon_128x128.webp',
         sizes: '128x128',
         type: 'image/webp'
       },
       {
-        src: '/apps/web/public/icons/vid_icon_144x144.webp',
+        src: '/icons/vid_icon_144x144.webp',
         sizes: '144x144',
         type: 'image/webp'
       },
       {
-        src: '/apps/web/public/icons/vid_icon_152x152.webp',
+        src: '/icons/vid_icon_152x152.webp',
         sizes: '152x152',
         type: 'image/webp'
       },
       {
-        src: '/apps/web/public/icons/vid_icon_192x192.webp',
+        src: '/icons/vid_icon_192x192.webp',
         sizes: '192x192',
         type: 'image/webp',
         purpose: 'any'
       },
       {
-        src: '/apps/web/public/icons/vid_icon_256x256.webp',
+        src: '/icons/vid_icon_256x256.webp',
         sizes: '256x256',
         type: 'image/webp'
       },
       {
-        src: '/apps/web/public/icons/vid_icon_384x384.webp',
+        src: '/icons/vid_icon_384x384.webp',
         sizes: '384x384',
         type: 'image/webp'
       },
       {
-        src: '/apps/web/public/icons/vid_icon_512x512.webp',
+        src: '/icons/vid_icon_512x512.webp',
         sizes: '512x512',
         type: 'image/webp',
         purpose: 'maskable'
@@ -71,7 +71,7 @@ export default function manifest(): MetadataRoute.Manifest {
     ],
     screenshots: [
       {
-        src: '/apps/web/public/screenshots/vid_screenshots_main.webp',
+        src: '/screenshots/vid_screenshots_main.webp',
         sizes: '540x720',
         type: 'image/webp'
       }
@@ -82,7 +82,14 @@ export default function manifest(): MetadataRoute.Manifest {
         short_name: '경매',
         description: '경매 메인으로 바로 이동',
         url: '/auction',
-        icons: [{ src: '/icons/auction.png', sizes: '96x96', type: 'image/png' }]
+        icons: [{ src: '/icons/auction.png', sizes: '96x96', type: 'image/webp' }]
+      },
+      {
+        name: '내가 쓴 스토리 바로가기',
+        short_name: '사연',
+        description: '내가 쓴 스토리로 바로 이동',
+        url: '/mypage/episodes',
+        icons: [{ src: '/icons/auction.png', sizes: '96x96', type: 'image/webp' }]
       }
     ]
   };
